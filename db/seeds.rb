b5a71246346ed0afd38d4b0f731cb6e4cb9fc02c
Kaley La Porta & Brittany Baranowski <pair+klaporta415.minigiraffe23@devbootcamp.com>
2017-05-18 16:05:15 -0500

    Database seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,52 +10,57 @@ end
   Answer.create(text: Faker::StarWars.wookie_sentence, question_id: Faker::Number.between(1,10), user_id: Faker::Number.between(1,10))
 end
 
-question_comments = 10.times do
-  Comment.new(text: Faker::StarWars.quote, user_id: Faker::Number.between(1,10))
+question_comments = []
+10.times do
+  question_comments << Comment.new(text: Faker::StarWars.quote, user_id: Faker::Number.between(1,10))
 end
 
-answer_comments = 10.times do
-  Comment.new(text: Faker::StarWars.quote, user_id: Faker::Number.between(1,10))
+answer_comments = []
+10.times do
+  answer_comments << Comment.new(text: Faker::StarWars.quote, user_id: Faker::Number.between(1,10))
 end
 
 question_comments.each do |comment|
-  user = User.find(Faker::Numer.between(1, 10))
+  user = User.find(Faker::Number.between(1, 10))
   user.comments << comment
   comment.save
 end
 
 answer_comments.each do |comment|
-  user = User.find(Faker::Numer.between(1, 10))
+  user = User.find(Faker::Number.between(1, 10))
   user.comments << comment
   comment.save
 end
 
-answer_votes = 5.times do
-  Vote.new([1, -1].sample)
+answer_votes = []
+5.times do
+  answer_votes << Vote.new(value: [1, -1].sample, user_id: Faker::Number.between(1,10))
 end
 
-comment_votes = 5.times do
-  Vote.new([1, -1].sample)
+comment_votes = []
+5.times do
+  comment_votes << Vote.new(value: [1, -1].sample, user_id: Faker::Number.between(1,10))
 end
 
-question_votes = 5.times do
-  Vote.new([1, -1].sample)
+question_votes = []
+5.times do
+  question_votes << Vote.new(value: [1, -1].sample, user_id: Faker::Number.between(1,10))
 end
 
 answer_votes.each do |vote|
-  answer = Answer.find(Faker::Numer.between(1, 10))
+  answer = Answer.find(Faker::Number.between(1, 10))
   answer.votes << vote
   vote.save
 end
 
 comment_votes.each do |vote|
-  comment = Comment.find(Faker::Numer.between(1, 10))
+  comment = Comment.find(Faker::Number.between(1, 10))
   comment.votes << vote
   vote.save
 end
 
 question_votes.each do |vote|
-  question = Question.find(Faker::Numer.between(1, 10))
+  question = Question.find(Faker::Number.between(1, 10))
   question.votes << vote
   vote.save
 end
