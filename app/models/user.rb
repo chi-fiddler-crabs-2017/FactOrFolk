4dbe23325c0476e27a218fa821d885df640ec01d
Jack Howard <johncarverhoward@gmail.com>
2017-05-18 15:50:06 -0500

    update layout file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
   has_many :votes
+
+  def password
+    @password ||= BCrypt::Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.hashed_password = @password
+  end
+
+  def authenticate(email, password)
+    authentic_email = email == self.email
+    authentic_pwd = BCrypt::Password.new(self.hashed_password) == password
+    authentic_pwd && authentic_email
+    end
+
 end
