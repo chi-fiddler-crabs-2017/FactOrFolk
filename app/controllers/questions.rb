702a4914e992c286dbd6d770655f34f1a431a4e5
Jack Howard <johncarverhoward@gmail.com>
2017-05-19 16:30:23 -0500

    add comment form and post routes on question controller

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -8,3 +8,13 @@ get '/questions/:id' do
   @answers = @question.answers
   erb :'questions/show'
 end
+
+post '/comments' do
+  # p params[:comment]
+  @user = User.last
+
+  @comment = Comment.new(text: params[:comment], user_id: @user.id )
+  @question = Question.new()
+  p @comment
+
+end
