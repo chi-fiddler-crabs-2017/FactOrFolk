ba2a9b3e8e7ac17bed1243319205d9e237868f40
Jack Howard <johncarverhoward@gmail.com>
2017-05-19 15:32:32 -0500

    login bcrypt authentication fixed

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -5,8 +5,11 @@ end
 post "/sessions" do
   @user = User.find_by(email: params[:email])
   p @user
+  p @user.email
+
 
   if @user && @user.authenticate(@user.email, @user.password)
+    p "hello"
     session[:user_id] = @user.id
      redirect "/users/#{@user.id}" # goes back to whatever homepage
   else
--- app/models/user.rb
+++ app/models/user.rb
@@ -20,8 +20,8 @@ class User < ActiveRecord::Base
   end
 
   def authenticate(email, password)
-    authentic_email = (email == self.email)
-    authentic_pwd = BCrypt::Password.new(self.hashed_password) == password
+    authentic_email = email == self.email
+    authentic_pwd = self.hashed_password == password
     authentic_pwd && authentic_email
   end
 end
