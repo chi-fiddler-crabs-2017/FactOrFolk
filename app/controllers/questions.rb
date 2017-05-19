e7e1dadb322094b6ebede236a9502e10b79220fa
Jack Howard <johncarverhoward@gmail.com>
2017-05-19 18:15:10 -0500

    append a comment to a question

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,18 +3,23 @@ get '/questions' do
   erb :'questions/index'
 end
 
+post '/questions/:id/comments' do
+  if logged_in? && current_user
+
+  @question = Question.find(params[:id])
+  @comment = Comment.new(text: params[:comment], user_id: current_user.id )
+  @question.comments << @comment
+  @comment.save
+  else
+  end
+  redirect "/questions/#{@question.id}"
+end
+
 get '/questions/:id' do
   @question = Question.find(params[:id])
   @answers = @question.answers
+  @comments = @question.comments
   erb :'questions/show'
 end
 
-post '/comments' do
-  # p params[:comment]
-  @user = User.last
 
-  @comment = Comment.new(text: params[:comment], user_id: @user.id )
-  @question = Question.new()
-  p @comment
-
-end
