8e3d46b95e239730a590fc3f3f18b6b0b5e7859f
Jack Howard <johncarverhoward@gmail.com>
2017-05-20 12:55:22 -0500

    got the answer of the submit html to return

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -68,13 +68,20 @@ end
 post '/questions/:id/answer' do
   @question = Question.find(params[:id])
   @answer = Answer.new(user_id: current_user.id, text: params[:text])
-  @question.answers << @answer
-  if @answer.save
-    redirect "/questions/#{@question.id}"
+
+  if request.xhr?
+    erb :'_submit_answer', layout: false
   else
-    @errors = @answer.errors.full_messages
-    erb :'questions/show'
+    redirect "/questions/#{@question.id}"
   end
+
+  # @question.answers << @answer
+  # if @answer.save
+  #   redirect "/questions/#{@question.id}"
+  # else
+  #   @errors = @answer.errors.full_messages
+  #   erb :'questions/show'
+  # end
 end
 
 post '/questions/:id/vote' do
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,33 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $('#answer-form').on('submit', function(event) {
+    event.preventDefault();
+    console.log('hi');
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $.ajax({
+    method: 'POST',
+    url: $('#answer-form').attr('action'),
+    data: $('#answer-form').serialize()
+  })
+
+  .done(function(response) {
+      console.log(response);
+      $('#answer-form').html(response)
+      // returns html for next page response
+    });
+  });
 });
+
+
+
+
+  // find the right place to listen to
+  // add and on submit function
+  // stop the default action
+  // take in the link that was going to post
+  // using like attr
+  // reassign the spot where it's going to add to
+  // probably need to load a partial
+
+
+  // id we want to target answer-form
+
